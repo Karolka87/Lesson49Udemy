e9087d7098bae7f1d702d219fae2629f519aad94
Karolka87 <115976221+Karolka87@users.noreply.github.com>
2023-03-03 11:31:18 +0100

    downcasting and lowbiding

--- src/Lesson49Udemy.java
+++ src/Lesson49Udemy.java
@@ -1,10 +1,14 @@
 public class Lesson49Udemy {
     public static void main(String[] args) {
 
-        People[] people = new People[100];
+        People[] people = new People[4];
         people[0] = new Student();
         people[1] = new Employee("Marc", "Novak", "man", 37, 3700);
         people[2] = new Student("Bart", "Simpson", "man", 17);
+
+       /* for (int i = 0; i < people.length; i++) {
+            people[i].getDescription();
+        }*/
 
         for (People person : people) {
             if (person instanceof People) {
@@ -16,7 +20,10 @@ public class Lesson49Udemy {
         People s1 = people[0];
         System.out.println(e1.fistName + " " + e1.lastName);
         System.out.println(people[2].age);
+
+        ((Employee) e1).work();
     }
+
 }
 
 abstract class People {
@@ -58,6 +65,9 @@ class Employee extends People {
     @Override
     void getDescription() {
         System.out.println("Some cool (or not) descritption of " + getClass());
+    }
+    void work() {
+        System.out.println("Employee is currently working.");
     }
 
 }
